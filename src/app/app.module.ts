2f59a53d6f596fbae0081e6a34054cadb8747134
aasys_iw <naashishiw@gmail.com>
2019-09-09 23:40:43 +0545

    implemented login with bitbucket in angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import AuthllizerModule from '@authllizer/ngx';
+import {BackendAdapter} from '@authllizer/core';
+import {environment} from '../environments/environment';
+import BitbucketOAuth2 from 'authllizer-bitbucket-oauth2';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ToastrModule} from 'ngx-toastr';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    AppRoutingModule,
+    AuthllizerModule.forRoot({
+      adapter: BackendAdapter.extend({
+        baseUrl: `${environment.backendUrl}/rest-auth/bitbucket/`
+      }),
+      interceptList: [environment.backendUrl],
+      providers:{
+        bitbucket: BitbucketOAuth2.extend({
+          clientId: 'Your_Bitbucket_Client_Id '
+        }),
+      }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import {Authllizer} from '@authllizer/core';
+import {ToastrService} from 'ngx-toastr';
+import {Router} from '@angular/router';
+import {HttpErrorResponse} from '@angular/common/http';
+
+export interface ISignInUser {
+  email?: string;
+  password?: string;
+}
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  user: ISignInUser = {};
+
+  constructor(private auth: Authllizer, private toastr: ToastrService, private router: Router) { }
+
+  ngOnInit() {
+  }
+  signIn() {
+    this.auth.signIn(this.user)
+      .then(() => {
+        this.toastr.success('You have successfully signed in!');
+        this.router.navigateByUrl('/');
+      })
+      .catch(({error,message,status}:HttpErrorResponse) => {
+        this.toastr.error(typeof error === 'object' && error.message ? error.message : message, status as any);
+      });
+  }
+  authenticate(provider: string) {
+    this.auth.authenticate(provider)
+      .then(() => {
+        this.toastr.success('You have successfully signed in with ' + provider + '!');
+        this.router.navigateByUrl('home/');
+      })
+      .catch((response: Error | HttpErrorResponse) => {
+        if ((response as HttpErrorResponse).error) {
+          let {error} = (response as HttpErrorResponse);
+          this.toastr.error(typeof error === 'object' && error.message  ? error.message : error);
+        } else if (!(response as HttpErrorResponse).message) {
+          this.toastr.error(response.message);
+        } else {
+          this.toastr.error(response as any);
+        }
+      });
+  };
+
+}
