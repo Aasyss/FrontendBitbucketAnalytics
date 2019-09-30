eea0d8c53161b6f5822655df8b72fd5dc724296c
Prabin Shrestha <shprabiniw@gmail.com>
2019-09-30 07:54:06 +0545

    Added table in charts for displaying total commits in a day by a user

--- src/app/layout/charts/charts.component.ts
+++ src/app/layout/charts/charts.component.ts
@@ -14,6 +14,9 @@ export class ChartsComponent implements OnInit {
   userCommits:any;
   userDateCommits:any;
   dateCommits:any;
+  counter:number;
+  // @ts-ignore
+  pushedItems:{[label:string]:[]}={};
   constructor(private analyticsService:AnalyticsService, private router:ActivatedRoute) {}
 
     // Doughnut
@@ -109,14 +112,28 @@ export class ChartsComponent implements OnInit {
         this.analyticsService.getUserDateCommits(slug).subscribe((data)=> {
           this.userDateCommits = data;
           console.log("Data",data);
-          for(let date in this.userDateCommits) {
-            console.log(date);
-            this.lineChartLabels.push(date);
-          }
 
-          console.log("date",this.lineChartLabels)
+          // for(let date in this.userDateCommits) {
+          //   this.counter=0;
+          //
+          //   this.lineChartLabels.push(date);
+          // //  userDateCommits:{[label:string],data:[]};
+          //   for(let k of Object.keys(this.userDateCommits[date][this.counter])){
+          //     console.log('key',k);
+          //     if(!(k in this.pushedItems)){
+          //       console.log("Boom",this.userDateCommits[date][this.counter][k]);
+          //       // @ts-ignore
+          //       this.pushedItems.label[k]=this.pushedItems.data.push(this.userDateCommits[date][this.counter][k]);
+          //     }else{
+          //
+          //     }
+          //     this.counter = this.counter+1
+          //   }
+          //
+          //
+          // }
 
-            // { data: [65, 59, 80, 81, 56, 55, 40], label: 'user' },
+          // { data: [65, 59, 80, 81, 56, 55, 40], label: 'user' },
         });
       }
 
